acd46831ba8015a3d6d7aab7f298f39abe0e751d
Dan <ngthdan0811@gmail.com>
2023-05-05 09:43:10 +0700

    animate and interactive

--- 2-canvas-tutor.js
+++ 2-canvas-tutor.js
@@ -1,13 +1,12 @@
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 
 // if (!canvas)  return"";
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
 // create a ton of methods.
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 
 // context.fillRect(x, y, width, height);
 
@@ -43,38 +42,70 @@ const context = canvas.getContext('2d');
 //     context.stroke();
 // }
 
+function Circle(x, y, dx, dy, radius) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
 
-let x = 200;
-let y = 200;
-let radius = 30;
-let dx = 4;
-let dy = 4
-
-const animate = () => {
-    // let time = performance.now();
-    // console.log(time);
-    requestAnimationFrame(animate);
-
-    // Xóa các hình đã tạo trước đó, x, y là vị trí bắt đầu, w, h là kích thước cần clear.
-    context.clearRect(0,0, innerWidth, innerHeight);
-
+  this.draw = function () {
     context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2, false);
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     context.strokeStyle = "blue";
     context.stroke();
+    context.fill();
+    context.fillStyle = "rgba(123,124,11, 0.3)";
+  };
 
-    if (x + radius >= window.innerWidth || x - radius < 0) {
-        dx = -dx;
+  this.update = function () {
+    if (this.x + this.radius >= window.innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
     }
 
-    if (y + radius >= window.innerHeight || y - radius < 0) {
-        dy = -dy;
+    if (
+      this.y + this.radius >= window.innerHeight ||
+      this.y - this.radius < 0
+    ) {
+      this.dy = -this.dy;
     }
 
-    x += dx;
-    y += dy;
+    this.x += this.dx;
+    this.y += this.dy;
 
+    this.draw();
+  };
 }
+
+// let x = Math.random() * innerWidth;
+// let y = Math.random() * innerHeight;
+// let radius = 30;
+// let dx = (Math.random() - 0.5) * 4;
+// let dy = (Math.random() - 0.5) * 4;
+
+let circleArrs = [];
+for (let i = 0; i < 100; i++) {
+  let radius = 30;
+  let x = Math.random() * (innerWidth - radius * 2) + radius;
+  let y = Math.random() * (innerHeight - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 1;
+  let dy = (Math.random() - 0.5) * 1;
+  circleArrs.push(new Circle(x, y, dx, dy, radius));
+  // let circle = new Circle(200, 200, 3, 3, 30);
+}
+
+const animate = () => {
+  // let time = performance.now();
+  // console.log(time);
+  requestAnimationFrame(animate);
+
+  // Xóa các hình đã tạo trước đó, x, y là vị trí bắt đầu, w, h là kích thước cần clear.
+  context.clearRect(0, 0, innerWidth, innerHeight);
+
+  circleArrs.forEach((circle) => {
+    circle.update();
+  });
+};
 
 animate();
 
